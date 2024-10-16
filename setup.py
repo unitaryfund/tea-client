5328f978a16f37178c250a72045ebbd9d851e662
vprusso <vincentrusso1@gmail.com>
2024-10-16 10:08:58 -0400

    Updating versions for tea-client fork.

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
     license="Apache-2.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=io.open("requirements.txt").read().splitlines(),
+    #install_requires=io.open("requirements.txt").read().splitlines(),
 )
